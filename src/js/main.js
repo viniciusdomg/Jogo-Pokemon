a2fb7514ef525207bc424f4159379320c5b843ae
viniciusdomg <viniciusmgomes2005@hotmail.com>
2023-07-24 14:11:43 -0300

    Primeiro commit

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+const cont = document.querySelector('.container');
+
+
+function paginaJogar(){
+    window.location.href='jogar.html';
+}
+
+function paginaPokemon(){
+    window.location.href='pokemons.html';
+}
+
+function paginaCreditos(){
+    window.location.href='creditos.html';
+}
+
+function voltar(){
+    window.location.href='index.html';
+}
+
+//}
+
+/*function mostrarMenu() {
+    fetch('https://pokeapi.co/api/v2/pokemon/', {method: 'GET'})
+        .then(resp => resp.json())
+        .then(function(json){
+            console.log(json);
+    
+            //esse "results" é onde está localizado os dados da API
+            json.results.map(function(results){ 
+                cont.innerHTML +=`
+                <div class="card"> <img src=` +results.image+`> 
+                <strong><font size="5">`+results.name+` </font></strong><br>
+                `;
+        })
+    })
+}
+ mostrarMenu();*/
